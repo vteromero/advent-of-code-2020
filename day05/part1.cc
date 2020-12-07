330b55e9187350835a3dd2ee645109f81c36d89a
Vicente Romero <vteromero@gmail.com>
2020-12-07 16:50:22 +0000

    day 5 - new implementations

--- day05/part1.cc
+++ day05/part1.cc
@@ -1,51 +1,23 @@
+#include <algorithm>
 #include <iostream>
 #include <string>
-#include <vector>
 
 using namespace std;
 
-int row(const string& row_str) {
-  size_t len = row_str.length();
-  int low = 0;
-  int sz = 128;
-
-  for (size_t i = 0; i < len; i++) {
-    char c = row_str[i];
-    sz /= 2;
-    if (c == 'B') {
-      low += sz;
-    }
-  }
-
-  return low;
-}
-
-int column(const string& column_str) {
-  size_t len = column_str.length();
-  int low = 0;
-  int sz = 8;
-
-  for (size_t i = 0; i < len; i++) {
-    char c = column_str[i];
-    sz /= 2;
-    if (c == 'R') {
-      low += sz;
-    }
-  }
-
-  return low;
+void to_binary(string& bpass) {
+  replace(bpass.begin(), bpass.end(), 'F', '0');
+  replace(bpass.begin(), bpass.end(), 'B', '1');
+  replace(bpass.begin(), bpass.end(), 'L', '0');
+  replace(bpass.begin(), bpass.end(), 'R', '1');
 }
 
 int main() {
   int highest_id = 0;
 
   for (string bpass; getline(cin, bpass); ) {
-    string row_str = bpass.substr(0, 7);
-    string column_str = bpass.substr(7);
+    to_binary(bpass);
 
-    int r = row(row_str);
-    int c = column(column_str);
-    int id = r * 8 + c;
+    int id = stoi(bpass, nullptr, 2);
 
     highest_id = max(highest_id, id);
   }
--- day05/part2.cc
+++ day05/part2.cc
@@ -1,63 +1,35 @@
+#include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-int row(const string& row_str) {
-  size_t len = row_str.length();
-  int low = 0;
-  int sz = 128;
-
-  for (size_t i = 0; i < len; i++) {
-    char c = row_str[i];
-    sz /= 2;
-    if (c == 'B') {
-      low += sz;
-    }
-  }
-
-  return low;
-}
-
-int column(const string& column_str) {
-  size_t len = column_str.length();
-  int low = 0;
-  int sz = 8;
-
-  for (size_t i = 0; i < len; i++) {
-    char c = column_str[i];
-    sz /= 2;
-    if (c == 'R') {
-      low += sz;
-    }
-  }
-
-  return low;
+void to_binary(string& bpass) {
+  replace(bpass.begin(), bpass.end(), 'F', '0');
+  replace(bpass.begin(), bpass.end(), 'B', '1');
+  replace(bpass.begin(), bpass.end(), 'L', '0');
+  replace(bpass.begin(), bpass.end(), 'R', '1');
 }
 
 int main() {
   vector<bool> available_seats(1024, true);
+  int min_id = 1024, max_id = 0;
 
   for (string bpass; getline(cin, bpass); ) {
-    string row_str = bpass.substr(0, 7);
-    string column_str = bpass.substr(7);
+    to_binary(bpass);
 
-    int r = row(row_str);
-    int c = column(column_str);
-    int id = r * 8 + c;
+    int id = stoi(bpass, nullptr, 2);
+
+    min_id = min(min_id, id);
+    max_id = max(max_id, id);
 
     available_seats[id] = false;
   }
 
-  size_t i, j;
-
-  for (i = 0; (i < available_seats.size() && available_seats[i]); i++) {}
-  for (j = available_seats.size(); (j-- > 0 && available_seats[j]); ) {}
-
-  for (size_t k = i; k <= j; k++) {
-    if (available_seats[k]) {
-      cout << k << endl;
+  for (int i = min_id; i <= max_id; i++) {
+    if (available_seats[i]) {
+      cout << i << endl;
     }
   }
 
