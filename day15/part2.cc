a1722ef3129970d970e7cecbd983d3634785051e
Vicente Romero <vteromero@gmail.com>
2020-12-15 16:33:55 +0000

    day 15

---
+++ day15/part2.cc
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int main() {
+  unordered_map<int, int> prev_spoken;
+  int turn = 1, last_spoken;
+
+  for(string line; getline(cin, line, ','); ) {
+    last_spoken = stoi(line);
+    prev_spoken[last_spoken] = turn++;
+  }
+
+  for (; turn <= 30000000; turn++) {
+    auto found = prev_spoken.find(last_spoken);
+
+    int last_turn = turn - 1;
+    int prev_spoken_turn = (found == prev_spoken.end()) ? last_turn : found->second;
+    int diff = last_turn - prev_spoken_turn;
+
+    prev_spoken[last_spoken] = last_turn;
+
+    last_spoken = diff;
+  }
+
+  cout << last_spoken << endl;
+
+  return 0;
+}
